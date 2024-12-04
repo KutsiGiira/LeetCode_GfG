8b7d8eb0ae54c03607b6417ed0284943832b6be1
KutsiGiira <haytamboualkhours@gmail.com>
2024-12-04 16:40:44 +0100

    Missing Number

---
+++ Missing_Number/Missing.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int c = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != c++){
+                return i;
+            }
+        }
+        return c;
+    }
+}
